7289b73863661c978e91a4ec24ac5c4b9793408d
Max <skomaxim@yahoo.ca>
2018-02-13 22:09:54 -0500

    Started project

---
+++ model.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+
+class Application:
+    pass
+
+class Board:
+    """
+    This class holds information about the current situation and scenario in/of the game.
+    Eg. Objective (when human enters the information, computer randonmly generates it)
+    Player history. There will be 2 parts, a guess and a hint. The guess is the same thing as the objective (above).
+    Need to determine the status/state of the game. (Win, loss, still playing, etc)
+    """
+    pass
+
